461042191c50a7ce84379ba74039c39592f10f54
ncutLJD <962603220@qq.com>
2019-02-22 00:35:49 +0800

    线程安全性-原子性

--- src/main/java/com/mmall/concurrency/example/atomic/AtomicExample1.java
+++ src/main/java/com/mmall/concurrency/example/atomic/AtomicExample1.java
@@ -1,15 +1,18 @@
-package com.mmall.concurrency;
+package com.mmall.concurrency.example.atomic;
 
+import com.mmall.concurrency.annotations.ThreadSafe;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 @Slf4j
-public class ConcurrencyTest {
+@ThreadSafe
+public class AtomicExample1 {
 
     // 请求总数
     public static int clientTotal = 5000;
@@ -17,12 +20,12 @@ public class ConcurrencyTest {
     // 同时并发执行的线程数
     public static int threadTotal = 200;
 
-    public static int count = 0;
+    public static AtomicInteger count = new AtomicInteger(0);
 
     public static void main(String[] args) throws Exception{
         ExecutorService executorService = Executors.newCachedThreadPool();
         final Semaphore semaphore = new Semaphore(threadTotal);
-        final CountDownLatch countDownLatch = new CountDownLatch(count);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
         for (int i = 0;i < clientTotal; i++) {
             executorService.execute(() -> {
                 try{
@@ -42,6 +45,6 @@ public class ConcurrencyTest {
     }
 
     private static void add(){
-        count++;
+        count.incrementAndGet();
     }
 }
